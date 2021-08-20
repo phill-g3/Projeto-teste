40f2038856b927597cec539eec1a7aa5c756c6ee
Phelipe Luis de Lima <ph.g3.lima@gmail.com>
2021-08-20 17:15:56 -0300

    projeto teste

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = DateTime.Now;
+            DateTime d2 = DateTime.UtcNow;
+            DateTime d3 = data.AddDays(1);
+            
+            TimeSpan t = new TimeSpan(0, 5, 22);
+            DateTime d4 = data.Subtract(t);
+
+            DateTime d5 = DateTime.Parse("2000-09-28T13:38:38Z");
+
+
+
+
+            Console.WriteLine(data);
+            Console.WriteLine(d2.ToLocalTime());
+            Console.WriteLine(d3);
+            Console.WriteLine(t);
+            Console.WriteLine(data.ToUniversalTime());
+            Console.WriteLine(d5);
+            Console.WriteLine(d5.ToUniversalTime());
+            
+        }
+    }
+}
